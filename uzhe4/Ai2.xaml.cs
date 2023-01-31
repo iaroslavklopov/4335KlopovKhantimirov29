368ced4a4681656d6de9a53f10c6798a065cda0f
Yar <iaroslavklopov@vk.com>
2023-01-31 17:59:21 +0300

    Added Okno aidar e

---
+++ uzhe4/Ai2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace uzhe4
+{
+    /// <summary>
+    /// Логика взаимодействия для Ai2.xaml
+    /// </summary>
+    public partial class Ai2 : Window
+    {
+        public Ai2()
+        {
+            InitializeComponent();
+        }
+    }
+}
